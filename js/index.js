0d3289c4b045ffff3b043f2342a28addba1f05ac
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2019-11-16 00:45:31 +0100

    Finish build

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 // Menu toggler
-const menuToggle = () =>{
+const menuToggle = () => {
     document.querySelector('.navbar_items').classList.toggle('navbar_show');
 }
 
-document.querySelector('.navbar_toggler').addEventListener('click',  menuToggle);
+document.querySelector('.navbar_toggler').addEventListener('click', menuToggle);
 
 // Education timeline show if it's in viewport
 const timelines = document.querySelectorAll('.education__timeline li');
 
 const isInViewport = el => {
     const rect = el.getBoundingClientRect();
-    return(
-        rect.top >= 0 && 
-        rect.left >= 0 && 
-        rect.bottom <= ( window.innerHeight || document.documentElement.clientHeight ) &&
-        rect.right <= ( window.innerWidth || document.documentElement.clientWidth )
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
 
-const showTimeline = () => 
-    timelines.forEach(timeline =>{
-        if(isInViewport(timeline)){
+const showTimeline = () =>
+    timelines.forEach(timeline => {
+        if (isInViewport(timeline)) {
             timeline.classList.add('show');
         }
     });
@@ -33,13 +33,13 @@ window.addEventListener('scroll', showTimeline);
 const animateExpertiseArea = () => {
     let tl = new TimelineLite();
 
-    if(isInViewport(document.querySelector('.expertise__areas'))){
-        tl.to(".ui", 0.5, {width: "78%"})
-          .to(".fe", 0.5, {width: "98%"}, "-=0.25")
-          .to(".fs", 0.5, {width: "60%"}, "-=0.25")
-          .to(".tw", 0.5, {width: "90%"}, "0")
-          .to(".tm", 0.5, {width: "80%"}, "-=0.45")
-          .to(".ch", 0.5, {width: "57%"}, "-=0.48");
+    if (isInViewport(document.querySelector('.expertise__areas'))) {
+        tl.to(".ui", 0.5, { width: "78%" })
+            .to(".fe", 0.5, { width: "98%" }, "-=0.25")
+            .to(".fs", 0.5, { width: "60%" }, "-=0.25")
+            .to(".tw", 0.5, { width: "90%" }, "0")
+            .to(".tm", 0.5, { width: "80%" }, "-=0.45")
+            .to(".ch", 0.5, { width: "57%" }, "-=0.48");
     }
 };
 
@@ -48,29 +48,29 @@ window.addEventListener('resize', animateExpertiseArea);
 window.addEventListener('scroll', animateExpertiseArea);
 
 //Header Animation
-TweenMax.from(".header_leftside", 0.9, {x:-600, opacity:0});
-TweenMax.from(".header_rightside", 0.9, { y:500, opacity:0, scale: 0, delay:0.2});
+TweenMax.from(".header_leftside", 0.9, { x: -600, opacity: 0 });
+TweenMax.from(".header_rightside", 0.9, { y: 500, opacity: 0, scale: 0, delay: 0.2 });
 
 // About me header Animation
-TweenMax.from (".more", 1.5, {x: 600, opacity: 0, ease: Back.easeOut.config(1.7) });
+TweenMax.from(".more", 1.5, { x: 600, opacity: 0, ease: Back.easeOut.config(1.7) });
 
 const animateAboutSection = () => {
-    if(isInViewport(document.querySelector('.about_me--image'))){
-        TweenMax.from('.about_me--image figure', 1.1, {x:-300});
+    if (isInViewport(document.querySelector('.about_me--image'))) {
+        TweenMax.from('.about_me--image figure', 1.1, { x: -300 });
     }
 
-    if(isInViewport(document.querySelector('.about_me--story'))){
+    if (isInViewport(document.querySelector('.about_me--story'))) {
         let text = document.querySelector('#text'), text2 = document.querySelector('#text2');
-        let newDom = '', newDom2 ='';
+        let newDom = '', newDom2 = '';
         let animationDelay = 0.5, animationDelay2 = 5;
 
 
-        for(let i = 0; i < text.innerText.length; i++){
-            newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i])+ '</span>';
+        for (let i = 0; i < text.innerText.length; i++) {
+            newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i]) + '</span>';
         }
 
-        for(let i = 0; i < text2.innerText.length; i++){
-            newDom2 += '<span class="char">' + (text2.innerText[i] == ' ' ? '&nbsp;' : text2.innerText[i])+ '</span>';
+        for (let i = 0; i < text2.innerText.length; i++) {
+            newDom2 += '<span class="char">' + (text2.innerText[i] == ' ' ? '&nbsp;' : text2.innerText[i]) + '</span>';
         }
 
         text.innerHTML = newDom;
@@ -78,19 +78,17 @@ const animateAboutSection = () => {
         let length = text.children.length;
         let length2 = text2.children.length;
 
-        for(let i = 0; i < length; i++)
-        {
+        for (let i = 0; i < length; i++) {
             text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
         }
 
-        for(let i = 0; i < length2; i++)
-        {
+        for (let i = 0; i < length2; i++) {
             text2.children[i].style['animation-delay'] = animationDelay2 * i + 'ms';
         }
     }
 
-    if(isInViewport(document.querySelector('.btn-group'))){
-        TweenMax.from('.btn-group button', 2, {opacity: 0, scale: 0, delay: 0.2});
+    if (isInViewport(document.querySelector('.btn-group'))) {
+        TweenMax.from('.btn-group button', 2, { opacity: 0, scale: 0, delay: 0.2 });
     }
 }
 
@@ -102,45 +100,45 @@ const menuContainer = document.querySelector('.navbar_items');
 const menuLinks = menuContainer.getElementsByClassName('nav-btn');
 
 for (let i = 0; i < menuLinks.length; i++) {
-  menuLinks[i].addEventListener("click", function() {
-    let currentLink = document.getElementsByClassName("current");
-    currentLink[0].className = currentLink[0].className.replace(" current", "");
-    this.className += " current";
-  });
+    menuLinks[i].addEventListener("click", function () {
+        let currentLink = document.getElementsByClassName("current");
+        currentLink[0].className = currentLink[0].className.replace(" current", "");
+        this.className += " current";
+    });
 }
 
 //Testimonial Slideshow
-var slideInterval, auto = 'true',  slideIndex = 0;
+var slideInterval, auto = 'true', slideIndex = 0;
 
-const showSlides = (n) =>{
-    let i; 
+const showSlides = (n) => {
+    let i;
     const slides = document.getElementsByClassName('testimonial_slide');
     const imgControl = document.getElementsByClassName('client');
 
-    if(n > slides.length){ slideIndex = 1 }
-    if(n < 1){ slideIndex = slides.length }
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
 
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";        
+        slides[i].style.display = "none";
     }
 
     slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 } 
-    if(slideIndex < 0){ slideIndex = slides.length }
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    if (slideIndex < 0) { slideIndex = slides.length }
 
-    for(i = 0; i < imgControl.length; i++){
-        imgControl[i].className = imgControl[i].className.replace(" current","");
+    for (i = 0; i < imgControl.length; i++) {
+        imgControl[i].className = imgControl[i].className.replace(" current", "");
     }
 
     slides[slideIndex - 1].style.display = "block";
     imgControl[slideIndex - 1].className += " current";
 
-    slideInterval = setTimeout(showSlides, 10000);
+    slideInterval = setTimeout(showSlides, 6000);
 }
 
-const currentSlide = n =>{
+const currentSlide = n => {
     clearTimeout(slideInterval);
-    showSlides(slideIndex = n - 1); 
+    showSlides(slideIndex = n - 1);
 }
 
 showSlides();
